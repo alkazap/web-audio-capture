79fd385a7509b9aaa75985ed92acf2bcaf524f2c
Alena Kazakova <alena.kazakova@actionpower.kr>
2020-04-07 13:18:56 +0900

    dealing with gum

--- static/webclient.js
+++ static/webclient.js
@@ -86,7 +86,9 @@ function connect() {
   ws.addEventListener('close', () => {
     console.log('ws.onclose: WebSocket is closed now');
     if (mediaRecorder != null) {
-      mediaRecorder.stop();
+      if (mediaRecorder.state === 'recording') {
+        mediaRecorder.stop();
+      }
     }
     testButton.value = 'Start';
     recordButton.value = 'Start';
@@ -122,29 +124,50 @@ function connect() {
 }
 
 function record() {
-  // https://developers.google.com/web/fundamentals/media/recording-audio
-  if (navigator.mediaDevices) {
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then((stream) => {
-        mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm', ignoreMutedMedia: true, audioBitsPerSecond: 16000 });
-        mediaRecorder.addEventListener('dataavailable', (event) => {
-          if (testButton.disabled === true && recordButton.disabled === false) {
-            if (event.data && event.data.size > 0) {
-              console.log(`mediaRecorder.ondataavailable: event.data.size: ${event.data.size}`);
-              event.data.arrayBuffer().then((buffer) => ws.send(buffer));
-            }
-          }
-        });
-        mediaRecorder.addEventListener('error', (event) => {
-          console.log(`mediaRecorder.onerror: MediaRecorder error: ${event}`);
-        });
-      })
-      .catch((error) => {
-        console.log(`getUserMedia error: ${error.message}`);
-      });
-  } else {
-    console.log('getUserMedia not supported on your browser!');
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+  // Older browsers might not implement mediaDevices at all, so we set an empty object first
+  if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
   }
+  // Some browsers partially implement mediaDevices. We can't just assign an object
+  // with getUserMedia as it would overwrite existing properties.
+  // Here, we will just add the getUserMedia property if it's missing.
+  if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = (constraints) => {
+      // First get ahold of the legacy getUserMedia, if present
+      const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      // Some browsers just don't implement it - return a rejected promise with an error
+      // to keep a consistent interface
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      }
+
+      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+      return new Promise(((resolve, reject) => {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      }));
+    };
+  }
+  // https://developers.google.com/web/fundamentals/media/recording-audio
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then((stream) => {
+      mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm', ignoreMutedMedia: true, audioBitsPerSecond: 16000 });
+      mediaRecorder.addEventListener('dataavailable', (event) => {
+        if (testButton.disabled === true && recordButton.disabled === false) {
+          if (event.data && event.data.size > 0) {
+            console.log(`mediaRecorder.ondataavailable: event.data.size: ${event.data.size}`);
+            event.data.arrayBuffer().then((buffer) => ws.send(buffer));
+          }
+        }
+      });
+      mediaRecorder.addEventListener('error', (event) => {
+        console.log(`mediaRecorder.onerror: MediaRecorder error: ${event}`);
+      });
+    })
+    .catch((error) => {
+      console.log(`getUserMedia error: ${error.message}`);
+    });
 }
 
 window.addEventListener('load', () => {
